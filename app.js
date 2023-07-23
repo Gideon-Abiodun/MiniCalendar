676005f845a49d15771df92da19bcfd9f5960dd0
Gideon-Abiodun <skuriangideon@gmail.com>
2023-07-23 01:19:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+// console.log(today);
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const Months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
+
+date.innerHTML = (today.getDate()< 10 ? "0" : "") + today.getDate();
+day.innerHTML = weekDays [today.getDay()];
+month.innerHTML = Months [today.getMonth()];
+year.innerHTML = today.getFullYear();
